e4419207e7aa8b36d36035af1f2ea2d00526f724
shouvikantu <shouvikwu26@gmail.com>
2023-03-10 00:31:47 -0800

    added favicon, added bg img

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import google from "../../public/google.png"
 
 
+
 const Login = () => {
 //   const { currentUser, loginWithGoogle } = useAuth();
 //   const router = useRouter();
@@ -24,17 +25,20 @@ const Login = () => {
 //   };
 
   return (
-    <div className="bg-gray-800 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
-        <h1 className="text-3xl font-bold mb-4">Welcome</h1>
-        <h2 className="text-lg font-medium mb-4">Intramural Staff</h2>
-        <p className="text-gray-500 mb-4">Login with your school email</p>
-        <button className="flex items-center justify-center bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition-all">
-          <Image src={google} alt="Google Logo" width={24} height={24} className="mr-2" />
-          Sign in with Google
-        </button>
-      </div>
+    <div className="bg-cover bg-center bg-container">
+  <div className="bg-gray-800 bg-opacity-75 min-h-screen flex items-center justify-center">
+    <div className="bg-gray-300 p-8 rounded-lg shadow-lg max-w-sm w-full">
+      <h1 className="text-3xl font-bold mb-4">Welcome</h1>
+      <h2 className="text-lg font-medium mb-4">Intramural Staff</h2>
+      <p className="text-gray-700 mb-4">Login with your school email</p>
+      <button className="flex items-center justify-center bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition-all">
+        <Image src={google} alt="Google Logo" width={24} height={24} className="mr-2" />
+        Sign in with Google
+      </button>
     </div>
+  </div>
+</div>
+
   );
 };
 
