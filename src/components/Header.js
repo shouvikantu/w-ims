9e9b3b19a33969da406721c6178ed804873d5a66
shouvikantu <shouvikwu26@gmail.com>
2023-03-10 00:05:30 -0800

    updated the header & redesigned the eventlisting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,17 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-gray-900 text-white">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="flex items-center justify-between">
-          <a href="/" className="font-bold text-sm md:text-xl">
-            Intramurals - Willamette University
+      <div className="container mx-auto md:px-4 px-1 py-4 flex justify-between items-center">
+        <div className="flex items-center w-1/3 ml-8">
+          <a href="/" className="font-bold text-sm md:text-xl text-center">
+            Willamette University Intramurals 
           </a>
         </div>
-        <div className="items-center">
-          <button className="md:px-4 md:py-2 bg-white text-gray-900 font-semibold rounded-full p-2">
+        <div className="flex justify-evenly flex-3 ">
+          <button className="md:px-4 md:py-2 text-white font-semibold rounded-full p-1">
             Sign In
           </button>
-          <Link className='md:px-4 md:py-2 bg-white text-gray-900 font-semibold rounded-full p-2' href="/evl" > All Events</Link>
+          <Link className='md:px-4 mx-2 md:py-2  text-white font-semibold rounded-full p-1 text-center' href="/evl" > All Events</Link>
         </div>
       </div>
     </header>
--- src/pages/evl.js
+++ src/pages/evl.js
@@ -3,6 +3,7 @@ import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
 
 import Link from "next/link";
+import Header from "@/components/Header";
 
 const eventsRef = collection(db, "events");
 
@@ -22,32 +23,33 @@ function EventListing() {
   }, []);
 
   return (
-    <div className="bg-white p-4 rounded-lg shadow-lg">
-      <h1 className="text-2xl font-bold mb-4">Upcoming Events</h1>
-      {events.length > 0 ? (
-        <ul className="space-y-4">
-          {events.map((event) => (
-            <li key={event.id} className="border-b border-gray-300 pb-4">
-              <h2 className="text-lg font-bold">{event.eventName}</h2>
-              <p className="text-gray-600">
-                {event.eventDate} at {event.time}
-              </p>
-              <p className="text-gray-600">{event.eventLocation}</p>
-              <p className="mt-2">{event.eventDescription}</p>
-              <Link href={`/registration/${event.id}`}
-                 className="text-blue-500 hover:text-blue-700">
-                  Register for this event
-               
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-gray-500">No upcoming events.</p>
-      )}
-
+    <div>
+      <Header />
+      <div className="bg-gray-800 p-4 rounded-lg shadow-lg h-screen">
+  <h1 className="text-2xl font-bold text-white mb-4">Upcoming Events</h1>
+  {events.length > 0 ? (
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center">
+      {events.map((event) => (
+        <li key={event.id} className="bg-gray-900 rounded-lg shadow-lg p-4 h-[150px] w-[400px]">
+          <h2 className="text-lg font-bold text-white">{event.eventName}</h2>
+          <p className="text-gray-400">
+            {event.eventDate} at {event.time}
+          </p>
+          <p className="text-gray-400">{event.eventLocation}</p>
+          <p className="mt-2 text-gray-300">{event.eventDescription}</p>
+          <Link href={`/registration/${event.id}`} className="text-blue-500 hover:text-blue-300">
+            Register for this event
+          </Link>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p className="text-gray-400">No upcoming events.</p>
+  )}
+</div>
 
     </div>
+
   );
 }
 
