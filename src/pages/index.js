aec80a3d40146351d5892b939444c53243066b1a
shouvikantu <shouvikwu26@gmail.com>
2023-03-11 23:55:12 -0800

    cleaned the code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,3 @@
-// import Head from "next/head";
-// import Header from "@/components/Header";
-// import EventCreation from "@/components/EventCreation";
-// import Login from "@/components/Login";
-
-// export default function Home() {
-//   const user = null;
-
-//   return (
-//     <>
-//       <Head>
-//         <title>ITS</title>
-//         <meta
-//           name="description"
-//           content="A tracking & Management system for Willamette University's intramural Program"
-//         />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//       <div className="text-amber-800">
-//         {!user ? (
-//           <Login />
-//         ) : (
-//           <div>
-//             <Header />
-//             <EventCreation />
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
 import Head from "next/head";
 import Header from "@/components/Header";
 import EventCreation from "@/components/EventCreation";
