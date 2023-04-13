9f26ac8d5976a035b8ada609673908a105ef5429
shouvikantu <shouvikwu26@gmail.com>
2023-04-13 15:08:52 -0700

    Fixed event listing design & functionality

--- src/pages/evl.js
+++ src/pages/evl.js
@@ -30,23 +30,29 @@ function EventListing({onLogout}) {
     <div className="px-4">
       <h1 className="text-3xl font-bold text-white  mb-4 lg:mb-6">Upcoming Events</h1>
       {events.length > 0 ? (
-        <ul className="flex flex-wrap justify-center gap-6">
+        
+            <ul className="flex flex-wrap justify-center gap-6">
           {events.map((event) => (
-            <li key={event.id} className="bg-opacity-80 rounded-lg shadow-lg p-4 w-[300px] lg:p-6 bg-gray border-2">
-              <h2 className="text-xl font-bold text-white mb-2">{event.eventName}</h2>
-              <p className="text-gray-400 mb-2">
+            <div className="bg-white bg-opacity-80">
+              <li key={event.id} className=" rounded-lg shadow-lg p-4 w-[300px] lg:p-6 bg-gray border-2">
+              <h2 className="text-xl font-bold text-black mb-2 ">{event.eventName}</h2>
+              <p className="text-gray-900 mb-2">
                 {event.eventDate} at {event.time}
               </p>
-              <p className="text-gray-400 mb-4 lg:mb-6">{event.eventLocation}</p>
-              <p className="text-gray-300">{event.eventDescription}</p>
-              <Link href={`/registration/${event.id}`} className="inline-block mt-4 p-2 bg-blue-500 text-white font-bold py-2 px-4 rounded">
+              <p className="text-gray-900 mb-4 lg:mb-6">{event.eventLocation}</p>
+              <p className="text-black">{event.eventDescription}</p>
+              <Link href={`/registration/${event.id}`} className="inline-block mt-4 p-2 bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 Register for this event
               </Link>
             </li>
+            </div>
+            
           ))}
         </ul>
+        
+        
       ) : (
-        <p className="text-gray-400">No upcoming events.</p>
+        <p className="text-black bg-white opacity-90 p-4">No upcoming events.</p>
       )}
     </div>
 </div>
