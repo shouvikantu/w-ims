046fb572ed66db29112a6c436b54659bdc1e4d34
shouvikantu <shouvikwu26@gmail.com>
2023-04-17 15:39:22 -0700

    fixed event creation design

--- src/components/EventCreation.js
+++ src/components/EventCreation.js
@@ -43,10 +43,10 @@ const EventCreation = ({ user}) => {
             <h1 className="text-3xl text-center font-bold mb-4 text-white">
               Create a new event
             </h1>
-            <form className="space-y-4">
+            <form className="space-y-4 p-3">
               <div class="mb-4">
                 <label
-                  className="block text-white text-md font-bold mb-2"
+                  className="block text-white text-lg font-bold mb-2"
                   for="Event Name"
                 >
                   Event Name
@@ -61,7 +61,7 @@ const EventCreation = ({ user}) => {
                 />
               </div>
               <div>
-                <label className="block text-white text-md font-bold mb-2">
+                <label className="block text-white text-lg font-bold mb-2">
                   Date of the Event
                 </label>
                 <input
@@ -73,7 +73,7 @@ const EventCreation = ({ user}) => {
               </div>
 
               <div>
-                <label className="block text-white text-md font-bold mb-2">
+                <label className="block text-white text-lg font-bold mb-2">
                   Event Location
                 </label>
                 <input
@@ -85,11 +85,11 @@ const EventCreation = ({ user}) => {
                 />
               </div>
               <div>
-                <label className="block text-white text-md font-bold mb-2">
+                <label className="block text-white text-lg font-bold mb-2">
                   Event Description
                 </label>
                 <textarea
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full h-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   rows="4"
                   onChange={(e) => setEventDescription(e.target.value)}
                   value={eventDescription}
