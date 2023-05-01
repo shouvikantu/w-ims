0d781dcacce1025cdb55b98963ee2e685ce43230
shouvikantu <shouvikwu26@gmail.com>
2023-04-30 23:39:15 -0700

    email restriction resolved?

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,11 +2,9 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const allowedEmails = [
-    'dtii@willamette.edu',
+
     'sahmed@willamette.edu',
-    'hcheng@willamette.edu',
-    'mmheine@willamette.edu',
-    'gcallahan@willamette.edu',
+
     // ... more email addresses
   ];
 
@@ -19,15 +17,14 @@ export default NextAuth({
     ],
     callbacks: {
         async signIn(user, account, profile) {
-          // Check if the user's email address is in the allowed list
-          if (allowedEmails.includes(user.email)) {
-            // User is allowed, return true
-            return true;
-          } else {
-            // User is not allowed, return false
-            return false;
-          }
+          
+            if (allowedEmails.includes(user.email)) {
+              return false;
+            } else {
+              return true;
+            }
         },
       },
+    
     secret : process.env.NEXTAUTH_SECRET
 });
