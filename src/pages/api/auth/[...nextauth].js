bd9a47b74f7a5ad12498df96785f4f5642b816ee
shouvikantu <shouvikwu26@gmail.com>
2023-04-30 23:57:04 -0700

    fixed login error

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -16,14 +16,20 @@ export default NextAuth({
         }),
     ],
     callbacks: {
-        async signIn(user, account, profile) {
+        async signIn({ user, account, profile, email, credentials }) {
+        if(user.email === "sahmed@willamette.edu"){
+            const isAllowedToSignIn = true
+            if (isAllowedToSignIn) {
+                return true
+              } else {
+                // Return false to display a default error message
+                return alert("Unauthorized")
+                // Or you can return a URL to redirect to:
+                // return '/unauthorized'
+              }
+        }
           
-            if (allowedEmails.includes(user.email)) {
-              return false;
-            } else {
-              return true;
-            }
-        },
+        }
       },
     
     secret : process.env.NEXTAUTH_SECRET
